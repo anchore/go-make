20ccd7d7e9e38f68cf6769262fe27eb951283412
Keith Zantow <kzantow@gmail.com>
2025-08-17 22:13:55 -0400

    chore: optional code coverage to file
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- tasks/gotest/test.go
+++ tasks/gotest/test.go
@@ -34,20 +34,22 @@ func Tasks(options ...Option) Task {
 			}
 			args = append(args, selectPackages(cfg.IncludeGlob, cfg.ExcludeGlob)...)
 
-			coverageFile := ""
+			coverageFile := cfg.CoverageFile
 			if cfg.Coverage {
-				coverageDir, err := os.MkdirTemp(config.TmpDir, "cover-dir-")
-				if err == nil {
-					defer func() {
-						log.Error(os.RemoveAll(coverageDir))
-					}()
-					coverageDir, err = filepath.Abs(coverageDir)
+				if coverageFile == "" {
+					coverageDir, err := os.MkdirTemp(config.TmpDir, "cover-dir-")
 					if err == nil {
-						coverageFile = filepath.Join(coverageDir, "cover.out")
-						args = append(args, "-coverprofile", coverageFile)
-						args = append(args, "-covermode=atomic", "-coverpkg=./...", "-tags=coverage")
+						defer func() {
+							log.Error(os.RemoveAll(coverageDir))
+						}()
+						coverageDir, err = filepath.Abs(coverageDir)
+						if err == nil {
+							coverageFile = filepath.Join(coverageDir, "cover.out")
+						}
 					}
 				}
+				args = append(args, "-coverprofile", coverageFile)
+				args = append(args, "-covermode=atomic", "-coverpkg=./...", "-tags=coverage")
 			}
 
 			if cfg.Race {
@@ -58,7 +60,7 @@ func Tasks(options ...Option) Task {
 
 			Log("Done running %s tests in %v", cfg.Name, time.Since(start))
 
-			if coverageFile != "" {
+			if coverageFile != "" && cfg.CoverageFile == "" {
 				Log(" -------------- Coverage Report -------------- ")
 				Run("go tool cover", run.Args("-func", coverageFile), run.Stdout(os.Stderr))
 			}
@@ -67,12 +69,13 @@ func Tasks(options ...Option) Task {
 }
 
 type Config struct {
-	Name        string
-	IncludeGlob string
-	ExcludeGlob string
-	Verbose     bool
-	Coverage    bool
-	Race        bool
+	Name         string
+	IncludeGlob  string
+	ExcludeGlob  string
+	Verbose      bool
+	Coverage     bool
+	CoverageFile string
+	Race         bool
 }
 
 func defaultConfig() Config {
